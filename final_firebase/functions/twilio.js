9f6b58240ea59d5c8372c439eadb6ee53bd107c5
dev <manoj.s@foodhub.com>
2023-09-17 20:40:48 +0530

    firebase fix

--- final_firebase/functions/twilio.js
+++ final_firebase/functions/twilio.js
@@ -1,5 +1,5 @@
 const twilio = require('twilio');
 const accountSid = 'ACa7fb3b02fbf760469c7a0d493ad6f29f';
-const authToken = 'd447314f9df2a8ac6f12b51e55c980a1';
+const authToken = '0ea40366c41a8c8b2ef7f5f28cdbd146';
 
 module.exports = new twilio.Twilio(accountSid, authToken);
